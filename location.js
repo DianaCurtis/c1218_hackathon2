126c187a6e49379dce175e15d7cadedb2427d824
Diana Curtis <dianacurtis92@gmail.com>
2019-01-29 13:20:09 -0800

    weathere data class created

--- location.js
+++ location.js
@@ -12,6 +12,7 @@ class LocDataTemplate {
     }
 
     addEventHandlers() {
+        debugger;
         $('#accept').click(this.getLocation);
     }
 
---
+++ weather.js
@@ -0,0 +1,34 @@
+class WeatherData{
+
+   constructor(responseCity){
+       this.city=responseCity;
+   }
+
+    var ajaxWeatherOptions={
+        url: "http://api.openweathermap.org/data/2.5/forecast",
+        method:'get',
+        dataType:'json',
+        data:{
+            q: this.city,
+            APPID:'a43ebfad64f2c63ddaff84b2c095f1ff'
+        },
+        success: this.weatherDataFunctionSucces,
+        error: this.weatherDataFailure
+    };
+    $.ajax(ajaxWeatherOptions);
+
+    weatherDataFunctionSucces(response){
+        console.log('the response was given',response);
+    }
+    weatherDataFailure(response){
+        console.log('the error was given',response);
+     }
+
+
+
+}
+
+
+
+
+
