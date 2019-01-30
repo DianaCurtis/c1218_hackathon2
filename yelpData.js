d0b41a90905e4d3ad8c05f53abdade6e89f99fc0
anthonybo <twandeez@gmail.com>
2019-01-30 13:28:26 -0800

    Added functionality for multiple restaurants and linked the noButton to allow for displaying the next restaurant

--- yelpData.js
+++ yelpData.js
@@ -17,12 +17,18 @@ class YelpData{
         this.images = '';
         this.restaurantLat=0;
         this.restaurantLong=0;
+        this.currentBuis = null;
+        this.allBuisnesses = null;
+        this.mainImage = '';
+        this.business_id = '';
 
         this.clickHandler = this.clickHandler.bind(this);
         this.getData = this.getData.bind(this);
         this.yelpDataSuccess = this.yelpDataSuccess.bind(this);
         this.showUserSelection = this.showUserSelection.bind(this);
         this.functionToRunMap = this.functionToRunMap.bind(this);
+        this.updateUserSelection = this.updateUserSelection.bind(this);
+        this.getfullRestaurantData = this.getfullRestaurantData.bind(this);
 
         this.clickHandler();
     }
@@ -37,6 +43,7 @@ class YelpData{
         // $('.button-container-food-options').on('click', '.categ-button', this.getData);
         $('#yesButton').click(this.showUserSelection);
         $("#yesButton").click(this.functionToRunMap);
+        $('#noButton').click(this.updateUserSelection);
     }
 
     /** Called when user clicks on the yes button for a particular restaurant
@@ -91,6 +98,8 @@ class YelpData{
                 location: this.city,
                 term: foodType,
                 limit: 50,
+                radius: 15000,
+                top: false
             },
             success: this.yelpDataSuccess
         }
@@ -100,34 +109,18 @@ class YelpData{
     /** Function to be called upon receiving a server response, dynamically adds info to the DOM,
      * stores information about a restaraunt in constructor */
     yelpDataSuccess (response) {
-        this.restaurantName = response.name;
-        this.priceRating = response.price;
-        this.phoneNumber = response.phone;
-        this.reviewCount = response.review_count;
-        this.rating = response.rating;
-        this.images = response.photos;
-        this.restaurantLat=response.coordinates.latitude;
-        this.restaurantLong=response.coordinates.longitude;
-        console.log('lat',this.restaurantLat);
-        console.log('long',this.restaurantLong);
+        // console.log(response.businesses[0]);
+        this.allBuisnesses = response;
+        console.log(this.allBuisnesses);
 
+        this.currentBuis = this.allBuisnesses.businesses.shift();
+        console.log(this.allBuisnesses.businesses[0]);
 
+        console.log(this.currentBuis);
 
-        $('.display_category_options_page').remove();
-        $('.display_restaurant_data_page').removeClass('hide');
+        console.log(this.allBuisnesses);
 
-        $('#restaurantName').text(this.restaurantName);
-
-
-
-        $('#foodImages').append($('<img>').attr('src', response.image_url).addClass('main-image'));
-
-        var priceRatingDiv = $('<div>').text(this.priceRating);
-        var reviewCountDiv = $('<div>').text('Review Count: ' + this.reviewCount);
-        var ratingDiv = $('<div>').text('Rating: ' + this.rating);
-
-        $('.restaurant_info').append(priceRatingDiv,reviewCountDiv, ratingDiv);
-
+        this.renderBusiness();
     }
 
 
@@ -138,6 +131,65 @@ class YelpData{
         linkToMap.displayMap();
     }
 
+    updateUserSelection() {
+        console.log('In Update');
+        console.log(this.allBuisnesses);
+        this.currentBuis = this.allBuisnesses.businesses.shift();
+        console.log(this.currentBuis);
+
+        this.renderBusiness();
+
+    }
+
+    sendfullRestaurantData() {
+        var ajaxConfig = {
+            // url: 'https://danielpaschal.com/lfzproxies/yelpproxy.php',
+            url: 'http://localhost:8888/c1218_hackathon2/server/business_detail.php',
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                'apikey': '-oCFKpv8HndKWcXaU0uRS03PEI9muDUSEq5cX6W2rgNY9i2nPagmxiEXgJRJ_1y96vpJ2dEe3tBKzVBWzMez0OQPVgF0WUKFpPLRNvLpFfETwJNTXxkd-XOE6rZPXHYx',
+            },
+            data:{
+                business_id: this.business_id
+            },
+            success: this.getfullRestaurantData
+        }
+        $.ajax(ajaxConfig);
+    }
+
+    getfullRestaurantData(response) {
+        console.log('Full Restaurant Data');
+        console.log(response.photos);
+        this.images = response.photos;
+    }
+
+    renderBusiness () {
+        this.restaurantName = this.currentBuis.name;
+        this.priceRating = this.currentBuis.price;
+        this.phoneNumber = this.currentBuis.phone;
+        this.reviewCount = this.currentBuis.review_count;
+        this.rating = this.currentBuis.rating;
+        this.business_id = this.currentBuis.id;
+        this.mainImage = this.currentBuis.image_url;
+        this.restaurantLat = this.currentBuis.coordinates.latitude;
+        this.restaurantLong = this.currentBuis.coordinates.longitude;
+
+        this.sendfullRestaurantData();
+
+        $('.display_category_options_page').remove();
+        $('.display_restaurant_data_page').removeClass('hide');
+
+        $('#restaurantName').text(this.restaurantName);
+
+        $('#foodImages').empty().append($('<img>').attr('src', this.mainImage).addClass('main-image'));
+
+        var priceRatingDiv = $('<div>').text(this.priceRating);
+        var reviewCountDiv = $('<div>').text('Review Count: ' + this.reviewCount);
+        var ratingDiv = $('<div>').text('Rating: ' + this.rating);
+
+        $('.restaurant_info').empty().append(priceRatingDiv,reviewCountDiv, ratingDiv);
+    }
 
 }
 
