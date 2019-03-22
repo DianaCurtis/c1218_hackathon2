3f4b72875e6aea3ee0932fbf98659537fb543047
anthonybo <twandeez@gmail.com>
2019-03-21 18:23:30 -0700

    Removed a console log

--- location.js
+++ location.js
@@ -72,7 +72,6 @@ class LocDataTemplate {
   * The city, lattitude and longitude are passed into the instantiation of the YelpData
   * **/
     onResponseSuccess(response) {
-        console.log(response);
         this.city = response.city;
          if(this.city == null) {
              $('#accept').hide();
